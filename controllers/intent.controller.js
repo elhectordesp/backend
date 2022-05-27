13aa5fc2fe987df40b3ecb55e7755c1e343ec3d2
Héctor <hectordura74@gmail.com>
2022-05-27 20:26:42 +0200

    nuevos metodos de crear intentfinal e intent e pregunta

--- controllers/intent.controller.js
+++ controllers/intent.controller.js
@@ -6,6 +6,7 @@ const keyFilename = './config/tfg-hector-nmsv.json';
 const parent = 'projects/' + projectId + '/locations/global';
 const intentsClient = new dialogflow.IntentsClient({projectId, keyFilename});
 const contextClient = new dialogflow.ContextsClient({projectId, keyFilename});
+const sessionClient = new dialogflow.SessionsClient({projectId, keyFilename});
 const intentCtrl = {};
 
 
@@ -14,23 +15,88 @@ intentCtrl.pruebas = (req, res) => {
     res.status(200).send({ message: 'fliiiias' });
 }
 
-intentCtrl.createContext = async (req, res) => {
-  
-  let contexts = [{
-    "name": "projects/" + projectId + "/agent/sessions/" + 'prueba' + "/contexts/empezarCuestionario",
-    "lifespanCount": 1,
-    
-  }]
+intentCtrl.createIntentPregunta = async (req, res) => {
+  let i = 0;
+  const agentPath = intentsClient.projectAgentPath(projectId);
+    const displayName = req.body.displayName;
+    const respuestas = req.body.respuestas;
+    const respuestasFinales = [{text: {text: ['Hola']} }];
+    respuestas.forEach((respuesta) => {
+      let res = [JSON.stringify(respuesta)];
+      const messageText = {
+        text: res,
+      };
+      
+      const message = {
+        text: messageText,
+      };
+      respuestasFinales.push(message);
+    });
 
-  const createContextRequest = {
-    context: contexts
-  };
-  const [response] = await contextClient.createContext(contexts);
+    const trainingPhrasesParts = [
+        'Si',
+        'Sii gracias',
+        'Sii porfa',
+        'yes',
+        'yesssss',
+    ];
+    const trainingPhrases = [];
+      
+    trainingPhrasesParts.forEach(trainingPhrasesPart => {
+        const part = {
+            text: trainingPhrasesPart,
+        };
+
+        const trainingPhrase = {
+            type: 'EXAMPLE',
+            parts: [part],
+          };
+
+          trainingPhrases.push(trainingPhrase);
+    });
+
+    let inputContextNames = [];
+      inputContextNames.push('projects/tfg-hector-nmsv/agent/sessions/prueba/contexts/' + req.body.contextoEntrada);
+
+      let outputContexts = [];
+      let contextoSalida = {
+        name: "projects/tfg-hector-nmsv/agent/sessions/prueba/contexts/" + req.body.contextoSalida,
+        lifespanCount: 1,
+        parameters: {},
+      };
+      let contextoSalidaFin = {
+        name: "projects/tfg-hector-nmsv/agent/sessions/prueba/contexts/empezarCuestionario",
+        lifespanCount: 1,
+        parameters: {},
+      };
+      
+      outputContexts.push(contextoSalida);
+      outputContexts.push(contextoSalidaFin);
+
+      console.log(respuestasFinales);
+  
+      const intent = {
+        displayName: displayName,
+        trainingPhrases: trainingPhrases,
+        messages: respuestasFinales,
+        inputContextNames: inputContextNames,
+        outputContexts: outputContexts
+      };
+  
+      const createIntentRequest = {
+        parent: agentPath,
+        intent: intent,
+      };
+
+      const [response] = await intentsClient.createIntent(createIntentRequest);
+      console.log(`Intent ${response.name} created`);
+      res.status(200).send({ message: `Intent ${response.name} created` });
 }
 
 intentCtrl.createIntent = async (req, res) => {
+  console.log(req.body);
     const agentPath = intentsClient.projectAgentPath(projectId);
-    const displayName = 'FinConversacion';
+    const displayName = req.body.displayName;
     const messageTexts = ['Vale, vuelve a hablarme si quieres hacer un cuestionario. :)'];
     const trainingPhrasesParts = [
         'No',
@@ -60,15 +126,24 @@ intentCtrl.createIntent = async (req, res) => {
       };
 
       let inputContextNames = [];
-      inputContextNames.push('empezarCuestionario');
+      inputContextNames.push('projects/tfg-hector-nmsv/agent/sessions/prueba/contexts/empezarcuetsionari');
+
+      let outputContexts = [];
+      const pepe = {
+        name: "projects/tfg-hector-nmsv/agent/sessions/prueba/contexts/pruebadelanyo",
+        lifespanCount: 1,
+        parameters: {},
+      };
+      outputContexts.push(pepe);
       
   
       const intent = {
         displayName: displayName,
         trainingPhrases: trainingPhrases,
-        messages: [message],
+        messages: [message, message],
         endInteraction: true,
         inputContextNames: inputContextNames,
+        outputContexts: outputContexts
       };
   
       const createIntentRequest = {
@@ -80,6 +155,103 @@ intentCtrl.createIntent = async (req, res) => {
     const [response] = await intentsClient.createIntent(createIntentRequest);
     console.log(`Intent ${response.name} created`);
     res.status(200).send({ message: `Intent ${response.name} created` });
+    //res.status(200);
 }
+
+intentCtrl.createIntentFin = async (req, res) => {
+  const agentPath = intentsClient.projectAgentPath(projectId);
+  const displayName = 'FinConversacion';
+  const messageTexts = ['Vale, vuelve a hablarme si quieres hacer un cuestionario. :)'];
+  const trainingPhrasesParts = [
+      'No',
+      'no gracias',
+  ];
+  const trainingPhrases = [];
+    
+  trainingPhrasesParts.forEach(trainingPhrasesPart => {
+      const part = {
+          text: trainingPhrasesPart,
+      };
+
+      const trainingPhrase = {
+          type: 'EXAMPLE',
+          parts: [part],
+        };
+
+        trainingPhrases.push(trainingPhrase);
+  });
+
+  const messageText = {
+      text: messageTexts,
+    };
+
+    const message = {
+      text: messageText,
+    };
+
+    let inputContextNames = [];
+    inputContextNames.push('projects/tfg-hector-nmsv/agent/sessions/prueba/contexts/empezarCuestionario');   
+
+    const intent = {
+      displayName,
+      trainingPhrases,
+      messages: [message],
+      endInteraction: true,
+      inputContextNames,
+    };
+
+    const createIntentRequest = {
+      parent: agentPath,
+      intent,
+    };
+
+    // Create the intent
+  const [response] = await intentsClient.createIntent(createIntentRequest);
+  console.log(`Intent ${response.name} created`);
+  res.status(200).send({ message: `Intent ${response.name} created` });
+}
+
+intentCtrl.detectIntent = async () => {
+  let sessionId = "prueba";
+  let text = "JUGON";
+
+  // Create a new session
+  const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryParams:{
+        //List of context to be sent and activated before the query is executed
+        contexts:[
+            {
+                // The context to be sent and activated or overrated in the session 
+                name: `projects/${projectId}/agent/sessions/${sessionId}/contexts/CONTEXT_NAME`,
+                // The lifespan of the context
+                lifespanCount: 8
+              }
+        ]
+    },
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: text,
+        // The language used by the client (en-US)
+        languageCode: 'es',
+      },
+    },
+  };
+
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log('Detected intent');
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  console.log(`  Output Contexts: ${JSON.stringify(result.outputContexts)}`)
+}
+
+
 
 module.exports = intentCtrl;
--- routes/intent.routes.js
+++ routes/intent.routes.js
@@ -5,6 +5,8 @@ const IntentController = require('../controllers/intent.controller');
 const api = express.Router();
 
 api.get('/create-intent', IntentController.createIntent);
-api.get('/context', IntentController.createContext);
+api.get('/prueba', IntentController.detectIntent);
+api.get('/createIntentFin', IntentController.createIntentFin);
+api.get('/createIntentPregunta', IntentController.createIntentPregunta);
 
 module.exports = api;
