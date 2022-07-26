955e0df2dcc5406749341b8019da6dae51bb2069
Héctor <hectordura74@gmail.com>
2022-07-27 00:49:38 +0200

    s

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.post("/", function (req, res) {
               if (res8.data.buena) {
                   response += " Correcta!";
               }
-              response = "Se ha registrado tu respuesta <<" + res1 + ">>";
+              response += "Se ha registrado tu respuesta <<" + res1 + ">>";
               if (!res8.data.buena) {
                 response +=
                   " Vaya! Has fallado! La respuesta era: " +
