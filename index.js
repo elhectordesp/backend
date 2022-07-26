7fdf460047752d9dc4f4f6aa2284a34057e89264
Héctor <hectordura74@gmail.com>
2022-07-26 19:31:22 +0200

    primera prueba buena

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.post("/", function (req, res) {
               response =
                 "Se ha registrado tu respuesta <<" +
                 res1 +
-                ">> ¿Quieres seguir con el cuestionario? Vaya! Has fallado! La respuesta era: " + res8.data.respuestasCorrectas;
+                ">> ¿Quieres seguir con el cuestionario? Vaya! Has fallado! La respuesta era: " + res8.data.respuestasCorrectas[0].texto;
             }
 
             res.json({
